25173159b467b76ab390015cad4c069f112bf94e
victchang <81805655+victchang@users.noreply.github.com>
2022-07-09 20:56:09 +0800

    Initial commit

--- scripts/relation_test.sh
+++ scripts/relation_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 export master_port="tcp://127.0.0.1:10001"
-export gpu_num=1
-export CUDA_VISIBLE_DEVICES="0,1"
+export gpu_num=4
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
 
-export archive="/home/dcs405a/victchang/sgg/checkpoints/sgdet/X-101-FPN-MSDN-0708"
-export model="model_0019999.pth"
+export archive="/home/dcs405a/victchang/sgg/checkpoints/sgdet/X-101-FPN-MSDN"
+export model="model_final.pth"
 
 python tools/train_net.py --dist-url $master_port --num-gpus $gpu_num --eval-only --config-file $archive/config.yaml \
     MODEL.WEIGHTS $archive/$model \
