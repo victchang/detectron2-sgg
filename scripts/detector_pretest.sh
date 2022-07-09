204dff5bf8a7b79f36d8ad450a2d89dd690bcec6
victchang <81805655+victchang@users.noreply.github.com>
2022-07-09 20:55:24 +0800

    Initial commit

--- scripts/detector_pretest.sh
+++ scripts/detector_pretest.sh
@@ -6,6 +6,6 @@ export CUDA_VISIBLE_DEVICES="0,1,2,3"
 export archive="/home/dcs405a/victchang/sgg/checkpoints/detection/X-101-FPN/"
 export model="model_final.pth"
 
-python tools/train_net.py --dist-url $master_port --num-gpus $gpu_num --eval-only --config-file configs/detection/X-101-FPN.yaml \
+python tools/train_net.py --dist-url $master_port --num-gpus $gpu_num --eval-only --config-file $archive/config.yaml \
     MODEL.WEIGHTS $archive/$model \
     OUTPUT_DIR $archive/testing
