20a58a30cab7a13ca4a21db5561a3e874b854ca4
victchang <81805655+victchang@users.noreply.github.com>
2022-07-09 20:54:46 +0800

    Initial commit

--- scripts/detector_pretrain.sh
+++ scripts/detector_pretrain.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export master_port="tcp://127.0.0.1:3407"
-export gpu_num=2
-export CUDA_VISIBLE_DEVICES="2,3"
-export output_dir="./checkpoints/detection/X-101-FPN-0628"
+export master_port="tcp://127.0.0.1:10001"
+export gpu_num=4
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+export output_dir="./checkpoints/detection/X-101-FPN"
 
-python tools/train_net.py --dist-url $master_port --num-gpus $gpu_num --config-file configs/detection/X-101-FPN.yaml --resume \
+python tools/train_net.py --dist-url $master_port --num-gpus $gpu_num --config-file configs/detection/X-101-FPN.yaml \
     OUTPUT_DIR $output_dir
